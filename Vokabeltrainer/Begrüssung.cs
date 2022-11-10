e68dafbad07468ec67a4caa99b56ebd344a3d90c
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2022-11-10 11:17:10 +0100

    zusammengestellte Version

--- Vokabeltrainer/Begrüssung.cs
+++ Vokabeltrainer/Begrüssung.cs
@@ -10,7 +10,12 @@ namespace Vokabeltrainer
     {
         public static void Willkommen()
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Herzlich Willkommen");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        
+            Console.WriteLine("In diesem Programm können sie Vokabeln lernen. Dafür wurde im Hintergrund eine Txt-Datei erstellt(users.txt).\nBitte öffnen sie Datei, falls sie den zu lernden Wortschatz ändern möchten gehen sie bitte in diese Datei. \n Für das müssen sie die Vokabel so eingeben: Wort in angewonter Sprache;Wort in Fremdsprache (Guten Tag;Bonjour)");
             Console.ReadKey();
             Console.Clear();
         }
--- Vokabeltrainer/XyClass.cs
+++ Vokabeltrainer/XyClass.cs
@@ -10,54 +10,73 @@ namespace Vokabeltrainer
     {
         public static void Wörterabfrage()
         {
-          int i = 0;
-            int l = 0; 
-           List<string> FslWörter = new List<string>();
+            int i = 0;
+            int l = 0;
+            List<string> FslWörter = new List<string>();
+            string weitermachen;
 
             try
             {
-                if (!File.Exists("users.txt"))
-                    File.Create("users.txt").Close();
-
-                string[][] users = loadData("users.txt");
-                
-
-                foreach (var user in users)
+                do
                 {
-                    Console.WriteLine("-----");
-                    Console.WriteLine("Deutscheswort: " + user[0]);
-                    Console.Write("Fremdwort: ");
-                    string eingabe = Console.ReadLine();
-                    if (eingabe == user[1])
+                    if (!File.Exists("users.txt"))
+                        File.Create("users.txt").Close();
+
+                    string[][] users = loadData("users.txt");
+
+
+                    foreach (var user in users)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("Richtig");
-                        Console.ForegroundColor = ConsoleColor.White;                        
-                        Console.ReadKey();
-                        i++;
+                        Console.WriteLine("-----");
+                        Console.WriteLine("Deutscheswort: " + user[0]);
+                        Console.Write("Fremdwort: ");
+                        string eingabe = Console.ReadLine();
+                        if (eingabe == user[1])
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Richtig");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.ReadKey();
+                            i++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Falsch");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.ReadKey();
+                            l++;
+                            FslWörter.Add(user[1]);
+
+                        }
+
                     }
-                    else
+                    Console.WriteLine("Richtige: " + i + " Falsche: " + l);
+
+
+                    Console.WriteLine("Hier sind die immer noch zu lernenden Wörter:");
+
+                    foreach (var falsch in FslWörter)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Falsch");
-                        Console.ForegroundColor = ConsoleColor.White;                    
-                        Console.ReadKey();
-                        l++;
-                        FslWörter.Add(user[1]);
-
+                        Console.WriteLine(falsch);
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
-                    
-                }
-                Console.WriteLine("Richtige: "+ i + " Falsche: "+ l);
-                Console.ReadKey();
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine("-----------");
+                    do
+                    {
+                        Console.Write("Möchtest du es nochmal Lernen?[Ja/Nein]");
+                        weitermachen = Console.ReadLine();
+                        if (weitermachen != "Ja" || weitermachen != "Nein")
+                        {
+                            Console.WriteLine("Sie müssen Ja oder Nein eingeben");   
+                        }
 
+                    } while (weitermachen != "Ja" && weitermachen != "Nein");
+                } while (weitermachen == "Ja");
 
-
-                foreach(var falsch in FslWörter)
-                {
-                    Console.WriteLine(falsch);                    
-                }
-                Console.ReadKey();
 
 
 
